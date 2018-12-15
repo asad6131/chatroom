3d8580780dd50ea58a59163260aa07cddb3d861a
sam <asad.bsse2993@iiu.edu.pk>
2018-12-15 08:00:18 -0800

    greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,24 +14,43 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) =>{
   console.log('new user connected');
 
-
   //emit to single connection
   // socket.emit('newMessage', {
   //   from: 'mike',
   //   text : `hey, what's going on.`,
   //   createdAt: 1234567
 
+      //socket.emit from admin to new user
+      socket.emit('newMessage', {
+        from : 'Admin',
+        text : 'welcome to the chat app',
+        createdAt: new Date().getTime()
+      });
+   
+      // socket.broadcast.emit from admin to all users other than new 
+      socket.broadcast.emit('newMessage', {
+        from : 'Admin',
+        text: 'new user joined',
+        createdAt: new Date().getTime()
+      });
+  
 
   //here we are listening a new message coming from one user
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    
     //and then we are emitting the users message to all other users
-    io.emit('newMessage', {
-      from : message.from,
-      text : message.text,
-      createdAt : new Date().getTime()
-    }); 
+     io.emit('newMessage', {
+       from : message.from,
+       text : message.text,
+       createdAt : new Date().getTime()
+     }); 
+
+    //but we need to emit the message to all of the other users but not ourself.... so as a solution we are using broadcasting
+    // socket.broadcast.emit('newMessage', {
+    //   from : message.from,
+    //   text : message.text,
+    //   createdAt : new Date().getTime()
+    // }); 
   });
   socket.on('disconnect' , () =>{
     console.log('user was disconnected');
